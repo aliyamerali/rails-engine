3d4b42c8a2845cfc1e02f7bd54c6a16e76c0f074
Aliya Merali <merali4@gmail.com>
2021-07-16 19:23:47 -0600

    Add test for merchant get endpoint

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants API' do
+  describe 'index' do
+    it 'returns a list of all merchants with a default of 20 max and page 1' do
+      create_list(:merchant, 50)
+      
+      get '/apli/v1/merchants'
+
+      expect(response).to be_successful
+      merchants = JSON.parse(response.body)
+
+      expect(merchants.count).to eq(20)
+      expect(merchants.first.id).to eq(Merchant.all.first.id)
+    end
+  end
+end
