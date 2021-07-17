e1e7930e18fceb7e5a5c9f2694b5867e43a6ead2
Aliya Merali <merali4@gmail.com>
2021-07-17 06:56:04 -0600

    Add tests for pagination inheritance in models

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe Merchant, type: :model do
 
       merchants = Merchant.all
 
-      expect(merchants.paginate(10,2).first.id).to eq(11)
-      expect(merchants.paginate(20,1).first.id).to eq(1)
-      expect(merchants.paginate(15,4).first.id).to eq(46)
+      expect(merchants.paginate(10,2).first.id).to eq(Merchant.first.id + 10)
+      expect(merchants.paginate(20,1).first.id).to eq(Merchant.first.id)
+      expect(merchants.paginate(15,4).first.id).to eq(Merchant.first.id + 45)
     end
   end
 end
