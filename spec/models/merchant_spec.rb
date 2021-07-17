15fa4bdab31dd894b0d7a83f00bc841a071b6e43
Aliya Merali <merali4@gmail.com>
2021-07-16 20:03:57 -0600

    Add test and model method for pagination

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoices) }
     it { should have_many(:items).dependent(:destroy) }
   end
+
+  describe 'class methods' do
+    it '.pagination returns results for a given page with given per page rate' do
+      create_list(:merchant, 50)
+
+      merchants = Merchant.all
+
+      expect(merchants.paginate(10,2).first.id).to eq(11)
+      expect(merchants.paginate(20,1).first.id).to eq(1)
+      expect(merchants.paginate(15,4).first.id).to eq(46)
+    end
+  end
 end
