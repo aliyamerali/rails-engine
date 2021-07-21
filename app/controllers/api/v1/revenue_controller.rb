2f0ae38e719c84100034f239fdd3c5438292649b
Aliya Merali <merali4@gmail.com>
2021-07-20 19:12:44 -0600

    Add sad path test

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::RevenueController < ApplicationController
-
   def merchant_total_revenue
-    merchant = Merchant.find(params[:id])
-
-    revenue = merchant.invoices
-            .joins(:transactions, :invoice_items)
-            .where(transactions: {result: "success"})
-            .where(invoices: {status: "shipped"})
-            .sum('invoice_items.unit_price * invoice_items.quantity')
-
-   render json: RevenueSerializer.merchant_revenue(merchant, revenue)
+    if Merchant.exists?(params[:id])
+      merchant = Merchant.find(params[:id])
+      revenue = merchant.invoices
+              .joins(:transactions, :invoice_items)
+              .where(transactions: {result: "success"})
+              .where(invoices: {status: "shipped"})
+              .sum('invoice_items.unit_price * invoice_items.quantity')
+      render json: RevenueSerializer.merchant_revenue(merchant, revenue)
+    else
+      render json: { response: 'Not Found' }, status: :not_found
+    end
   end
 end
