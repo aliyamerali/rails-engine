1b826ef85cc0e8a2195fa97635542a6c1beb3519
Aliya Merali <merali4@gmail.com>
2021-07-22 17:16:32 -0600

    Refactor item serializer to user fastjson gem

---
+++ app/serializers/item_serializer.rb
@@ -0,0 +1,26 @@
+class ItemSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :description, :unit_price, :merchant_id
+  # def self.format_items(items)
+  #   { data: items.map do |item|
+  #     { "id": item.id.to_s,
+  #       "type": 'item',
+  #       "attributes":
+  #       { "name": item.name,
+  #         "description": item.description,
+  #         "unit_price": item.unit_price,
+  #         "merchant_id": item.merchant_id } }
+  #   end }
+  # end
+  #
+  # def self.format_item(item)
+  #   { data:
+  #     { "id": item.id.to_s,
+  #       "type": 'item',
+  #       "attributes":
+  #       { "name": item.name,
+  #         "description": item.description,
+  #         "unit_price": item.unit_price,
+  #         "merchant_id": item.merchant_id } } }
+  # end
+end
