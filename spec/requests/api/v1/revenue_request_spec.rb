748f80324407a8b8becaa518867195c13a7b38b8
Aliya Merali <merali4@gmail.com>
2021-07-22 16:51:15 -0600

    Spec cleanup

--- spec/requests/api/v1/revenue_request_spec.rb
+++ spec/requests/api/v1/revenue_request_spec.rb
@@ -1,8 +1,5 @@
 require 'rails_helper'
 
-# HINT: Invoices must have a successful transaction and be
-# shipped to the customer to be considered as revenue.
-
 RSpec.describe 'Revenue API endpoints' do
   describe 'merchant revenue' do
     it 'returns the total revenue for a single merchant' do
